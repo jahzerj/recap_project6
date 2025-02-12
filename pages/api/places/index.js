529bde74388795c4f7e01455a449430bc9225510
Jahaziel <jahzerahj@gmail.com>
2025-02-12 17:01:26 +0100

    added post to api/places

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,14 +4,25 @@ import Place from '@/db/models/Place';
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === 'GET') {
-    const places = await Place.find();
+  try {
+    if (request.method === 'GET') {
+      const places = await Place.find();
 
-    response.status(200).json(places);
+      response.status(200).json(places);
+      return;
+    }
+
+    if (request.method === 'POST') {
+      const placeData = request.body;
+      await Place.create(placeData);
+
+      response.status(201).json({ status: 'Place createdff' });
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: 'Internal Server Error.' });
     return;
   }
-
-  // if(request.method === 'POST') {
-  //   const place = await
-  // }
+  response.status(405).json({ status: 'Method not allowed.' });
 }
