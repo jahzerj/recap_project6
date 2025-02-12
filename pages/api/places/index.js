83a69ae7c2dcd89338ae42cf606d778a8f1d2ea8
Jahaziel <jahzerahj@gmail.com>
2025-02-12 16:50:13 +0100

    added post request

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -4,19 +4,14 @@ import Place from '@/db/models/Place';
 export default async function handler(request, response) {
   await dbConnect();
 
-  try{
+  if (request.method === 'GET') {
+    const places = await Place.find();
 
-    if(request.method === "GET"){
-      const places = await Place.find();
-
-      response.status(200).json(places);
-      return;
-    }
-
-    if(request.method === 'POST') {
-      const place = await 
-    }
+    response.status(200).json(places);
+    return;
   }
 
-  
+  // if(request.method === 'POST') {
+  //   const place = await
+  // }
 }
