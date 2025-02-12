5fe69731503764ef8e6843e71bc35fd0576b1c00
Jahaziel <jahzerahj@gmail.com>
2025-02-12 15:45:50 +0100

    connected pages DB to index and slugpage

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,27 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from '@/db/connect';
+import Place from '@/db/models/Place';
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
+  try {
+    if (request.method === 'GET') {
+      const place = await Place.findById(id);
 
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
-    return;
+      if (!place) {
+        response.status(404).json({ status: 'Not found' });
+        return;
+      }
+
+      response.status(200).json(place);
+      return;
+    }
+
+    //if (request.method === 'DELETE")
+    //etc...
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: 'Internal Server Error.' });
   }
-
-  response.status(200).json(place);
 }
