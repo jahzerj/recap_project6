bb64ebc2c6c08cfa86605a2b067423ff69d9aa00
Jahaziel <jahzerahj@gmail.com>
2025-02-13 10:16:31 +0100

    adding comments

--- components/Comments.js
+++ components/Comments.js
@@ -24,61 +24,69 @@ export default function Comments({ placeId, locationName }) {
   const router = useRouter();
   const { isReady } = router;
   const { id } = router.query;
-  const { data: comments, mutate, isLoading, error } = useSWR(`/api/comments/${id}`);
+  const { data: comments, mutate, isLoading, error } = useSWR(`/api/comments/${placeId}`);
 
-  if (!isReady || isLoading || error) return <h2>Loading...</h2>;
+  if (!isReady || isLoading) return <h2>Loading...</h2>;
+  if (error) return <p>Error loading comments</p>;
 
   async function handleSubmitComment(event) {
     event.preventDefault();
-    const response = await fetch('api/comments', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, comment: commentText, placeId }),
-    });
 
-    if (response.ok) {
-      setCommentText('');
-      setName('');
+    async function handleSubmitComment(formData) {
+      // formData is now directly passed from the Form component
+      const response = await fetch('/api/comments/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ ...formData, placeId }), // Append placeId
+      });
+
+      if (response.ok) {
+        setCommentText('');
+        setName('');
+        mutate(`/api/comments/${placeId}`);
+      }
+      console.log('adding comment');
+    }
+
+    async function handleDeleteComment(_id) {
+      await fetch(`/api/comments/${placeId}?commentId=${_id}`, {
+        method: 'DELETE',
+      });
+
       mutate(`/api/comments/${placeId}`);
     }
-    console.log('adding comment');
+
+    if (error) return <p>Error loading comments</p>;
+    if (!comments) return <p>Loading...</p>;
+
+    return (
+      <Article>
+        <FormContainer onSubmit={handleSubmitComment}>
+          <Label htmlFor="name">Your Name</Label>
+          <Input type="text" name="name" placeholder="name" />
+          <Label htmlFor="comment">Your Comment</Label>
+          <Input type="text" name="comment" placeholder="comment here..." />
+          <StyledButton type="submit">Send</StyledButton>
+        </FormContainer>
+        {comments && (
+          <>
+            <h2>{comments.length} fans commented on this place:</h2>
+            {comments.map(({ _id, name, comment }) => {
+              return (
+                <Fragment key={_id}>
+                  <CommentText>
+                    <small>
+                      <strong>{name}</strong> commented on {locationName || 'this place'}
+                    </small>
+                  </CommentText>
+                  <span>{comment}</span>
+                  <StyledButton onClick={() => handleDeleteComment(_id)}>X</StyledButton>
+                </Fragment>
+              );
+            })}
+          </>
+        )}
+      </Article>
+    );
   }
-
-  async function handleDeleteComment(comment_id) {
-    await fetch(`/api/comments/${comment_Id}`, { method: 'DELETE' });
-    mutate(`/api/comments/${placeId}`);
-  }
-
-  if (error) return <p>Error loading comments</p>;
-  if (!comments) return <p>Loading...</p>;
-
-  return (
-    <Article>
-      <FormContainer onSubmit={handleSubmitComment}>
-        <Label htmlFor="name">Your Name</Label>
-        <Input type="text" name="name" placeholder="name" />
-        <Label htmlFor="comment">Your Comment</Label>
-        <Input type="text" name="comment" placeholder="comment here..." />
-        <StyledButton type="submit">Send</StyledButton>
-      </FormContainer>
-      {comments && (
-        <>
-          <h2>{comments.length} fans commented on this place:</h2>
-          {comments.map(({ _id, name, comment }) => {
-            return (
-              <Fragment key={_id}>
-                <CommentText>
-                  <small>
-                    <strong>{name}</strong> commented on {locationName}
-                  </small>
-                </CommentText>
-                <span>{comment}</span>
-                <StyledButton onClick={() => handleDeleteComment(_id)}>X</StyledButton>
-              </Fragment>
-            );
-          })}
-        </>
-      )}
-    </Article>
-  );
 }
